2f50a6af42c0d926502ac500a7665c4771816591
Minelay777 <98479359+Minelay777@users.noreply.github.com>
2022-08-19 20:50:42 +0300

    изменил проверку города

--- cli_city.py
+++ cli_city.py
@@ -33,8 +33,8 @@ UAH
 
 MY_APY_KEY = '1/A0CepAbSbSFXX4jk/lQw==8GpZICsyggRPEkPF'
 
+# city_name = 'gkgkh'
 
-# city_name = 'Odessa'
 
 def city_info(city_name: str, MY_APY_KEY: str):
     """
@@ -45,21 +45,22 @@ def city_info(city_name: str, MY_APY_KEY: str):
     """
     api_url = 'https://api.api-ninjas.com/v1/city?name={}'.format(city_name)
     resp_city = requests.get(api_url, headers={'X-Api-Key': MY_APY_KEY})  # запрос информации о городе
+    if resp_city.status_code == requests.codes.ok:
+        api_url = 'https://api.api-ninjas.com/v1/country?name={}'.format(resp_city.json()[0]['country'])
+        resp_country = requests.get(api_url, headers={'X-Api-Key': MY_APY_KEY})  # запрос информации о стране
 
-    api_url = 'https://api.api-ninjas.com/v1/country?name={}'.format(resp_city.json()[0]['country'])
-    resp_country = requests.get(api_url, headers={'X-Api-Key': MY_APY_KEY})  # запрос информации о стране
-    
-    if resp_city.status_code == requests.codes.ok and resp_country.status_code == requests.codes.ok:
-        print('--------------')
-        print(resp_city.json()[0]['name'])
-        print("")
-        print(resp_country.json()[0]['name'])
-        print(resp_country.json()[0]['currency']['code'])
-        print(int(resp_country.json()[0]['population']) * 1000)  # преобразуем в число и переводим в млн. человек
-        print('==============')
+        if resp_country.status_code == requests.codes.ok:
+            print('--------------')
+            print(resp_city.json()[0]['name'])
+            print("")
+            print(resp_country.json()[0]['name'])
+            print(resp_country.json()[0]['currency']['code'])
+            print(int(resp_country.json()[0]['population']) * 1000)  # преобразуем в число и переводим в млн. человек
+            print('==============')
+        else:
+            print("Error:", resp_country.status_code, resp_country.text)
     else:
-        print("Error:", resp_city.status_code, resp_city.text)
-        print("Error:", resp_country.status_code, resp_country.text)
+        print(f"Error: invalid city name. {city_name}", resp_city.status_code, resp_city.text)
 
 
 @click.command()  # подключаем возможность передачи данных из терминала
@@ -67,7 +68,6 @@ def city_info(city_name: str, MY_APY_KEY: str):
 # @click.option(help='After space input city name, like Kyiv')
 def main(city_name: str, api_key=MY_APY_KEY):
     """
-
     input city name, like Kyiv and
     return: information of city: country, population, currency code.
     """
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         main()
     except TypeError:
         print("System Error")
-    # except SyntaxError:
+        # except SyntaxError:
         print("System Error")
-    except IndexError:
-        print("Invalid city name")
+        # except IndexError:
+        print(f"Invalid city name")
